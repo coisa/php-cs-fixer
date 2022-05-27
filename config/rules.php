59d4821afc0040b28b52b13d6f99053bfcd1abf2
Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
2022-05-27 16:49:17 -0300

    Update rules

--- config/rules.php
+++ config/rules.php
@@ -1,6 +1,8 @@
 <?php
 
-/**
+declare(strict_types=1);
+
+/*
  * This file is part of coisa/php-cs-fixer.
  *
  * This source file is subject to the license that is bundled
@@ -11,24 +13,31 @@
  * @license   https://opensource.org/licenses/MIT MIT License
  */
 
-return [
-    '@DoctrineAnnotation'    => true,
-    '@PHP74Migration'        => true,
-    '@PHP74Migration:risky'  => true,
-    '@PHP80Migration'        => (float)PHP_VERSION >= 8,
-    '@PHP80Migration:risky'  => (float)PHP_VERSION >= 8,
-    '@PHP81Migration'        => (float)PHP_VERSION >= 8.1,
-    '@PSR12'                 => true,
-    '@PSR12:risky'           => true,
-    '@PhpCsFixer'            => true,
-    '@PhpCsFixer:risky'      => true,
+use PhpCsFixer\RuleSet\RuleSet;
+
+$ruleSet = new RuleSet([
+    '@DoctrineAnnotation'   => true,
+    '@PHP74Migration'       => true,
+    '@PHP74Migration:risky' => true,
+    '@PHP80Migration'       => (float) PHP_VERSION >= 8,
+    '@PHP80Migration:risky' => (float) PHP_VERSION >= 8,
+    '@PHP81Migration'       => (float) PHP_VERSION >= 8.1,
+    '@PSR12'                => true,
+    '@PSR12:risky'          => true,
+    '@PhpCsFixer'           => true,
+    '@PhpCsFixer:risky'     => true,
+]);
+
+$rules = $ruleSet->getRules();
+
+return array_merge($rules, [
     'align_double_arrow'     => true,
     'binary_operator_spaces' => [
         'operators' => [
             '='   => 'align',
             '=>'  => 'align_single_space_minimal',
-            '===' => 'align_single_space_minimal'
-        ]
+            '===' => 'align_single_space_minimal',
+        ],
     ],
     'blank_line_before_return'    => true,
     'class_attributes_separation' => true,
@@ -37,16 +46,14 @@ return [
         'spacing' => 'one',
     ],
     'date_time_immutable'              => false,
+    'declare_parentheses'              => true,
     'general_phpdoc_annotation_remove' => [
         'annotations' => [
             'author',
         ],
     ],
     'header_comment' => [
-        'comment_type' => 'PHPDoc',
-        'header'       => $header ?? '',
-        'location'     => 'after_open',
-        'separate'     => 'both',
+        'header' => $header ?? '',
     ],
     'heredoc_indentation'               => false,
     'mb_str_functions'                  => true,
@@ -58,4 +65,4 @@ return [
     'phpdoc_no_package'                 => false,
     'simplified_null_return'            => true,
     'trailing_comma_in_multiline_array' => true,
-];
+]);
